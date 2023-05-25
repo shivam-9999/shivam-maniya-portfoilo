8eacc5fb42e335c177cf05ba85c1e8b89fbdafc8
jackmillerdev <perramatthew2@gmail.com>
2023-05-24 23:16:39 -0400

    first committ

--- src/Asset/index.js
+++ src/Asset/index.js
@@ -1,6 +1,6 @@
 const AboutMe =
   " Hey! I am Shivam.I build interactive websites that run across platforms & devices";
 
-const hello = "hello";
+const hello = "helloo weort";
 
 export { AboutMe, hello };
